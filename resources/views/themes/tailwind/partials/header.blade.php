53aefcbc7e624a7035bcc4c4e0350fbffdc0cb1d
carlosg2 <212313+carlosg2@users.noreply.github.com>
2021-10-29 21:37:55 -0500

    logo

--- resources/views/themes/tailwind/partials/header.blade.php
+++ resources/views/themes/tailwind/partials/header.blade.php
@@ -13,7 +13,14 @@
             </div>
             <div class="flex justify-end flex-grow -my-2 -mr-2 md:hidden">
                 <button @click="mobileMenuOpen = true" type="button" class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
-                    <svg class="w-6 h-6" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="17.997" height="17.997" viewBox="0 0 17.997 17.997" fill="currentColor">
+							<g id="Group_38" data-name="Group 38" transform="translate(17.997) rotate(90)">
+								<rect id="Rectangle_241" data-name="Rectangle 241" width="3.9" height="8.825" transform="translate(0 2.758) rotate(-45)" />
+								<rect id="Rectangle_242" data-name="Rectangle 242" width="3.9" height="8.825" transform="translate(8.998 11.756) rotate(-45)" />
+								<rect id="Rectangle_243" data-name="Rectangle 243" width="3.9" height="8.825" transform="translate(2.758 17.997) rotate(-135)" />
+								<rect id="Rectangle_244" data-name="Rectangle 244" width="3.9" height="8.825" transform="translate(11.756 8.998) rotate(-135)" />
+							</g>
+					</svg>
                 </button>
             </div>
 
